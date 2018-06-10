bceea26a9fd0c03ed90fe28112eb24dd06ded989
iwilliams83 <indirajamurath@gmail.com>
2018-06-10 17:37:31 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,7 @@
 class Person
   attr_accessor :attributes
 
-  @@attributes = {}
-
   def initialize(attributes)
-    @@attributes = attributes
-    @@attributes.each {|key, value| self.send(("#{key}="), value)}
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
